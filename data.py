a422a09909c7c126905b4ffd4f2583ea39fc1b9e
Rithesh Kumar <ritheshkumar.95@gmail.com>
2018-09-13 18:08:13 -0400

    fixing denoising bug

--- data.py
+++ data.py
@@ -3,10 +3,15 @@ from torchvision import datasets, transforms
 
 
 def inf_train_gen(dataset, batch_size):
+    transf = transforms.Compose([
+        transforms.Resize(32),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
     loader = torch.utils.data.DataLoader(
         eval('datasets.%s' % dataset)(
             '../data/%s' % dataset, train=True, download=True,
-            transform=transforms.ToTensor()
+            transform=transf
         ), batch_size=64, drop_last=True
     )
     while True:
--- modules.py
+++ modules.py
@@ -17,96 +17,77 @@ def calc_gradient_penalty(netD, real_data, fake_data, lamda=.1):
 
 
 def calc_reconstruction(netE, data, sigma):
-    data.requires_grad_(True)
-    energy = netE(data)
+    noisy_data = data + torch.normal(0, torch.ones_like(data) * sigma)
+    noisy_data.requires_grad_(True)
+    energy = netE(noisy_data)
     score = torch.autograd.grad(
         outputs=energy, inputs=data,
         grad_outputs=torch.ones_like(energy),
         create_graph=True, retain_graph=True, only_inputs=True
     )[0]
-    return data - (sigma ** 2) * score
+    return noisy_data - (sigma ** 2) * score
 
 
 class Generator(nn.Module):
     def __init__(self, z_dim=128, dim=64):
         super(Generator, self).__init__()
-        self.preprocess = nn.Sequential(
-            nn.Linear(z_dim, 4 * 4 * 4 * dim),
-            nn.ReLU(True)
+        self.main = nn.Sequential(
+            nn.ConvTranspose2d(z_dim, dim * 4, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(dim * 4),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(dim * 4, dim * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(dim * 2),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(dim * 2, dim, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(dim),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(dim, 1, 4, 2, 1, bias=False),
+            nn.Tanh()
         )
-        self.block1 = nn.Sequential(
-            nn.ConvTranspose2d(4 * dim, 2 * dim, 5),
-            nn.ReLU(True)
-        )
-        self.block2 = nn.Sequential(
-            nn.ConvTranspose2d(2 * dim, dim, 5),
-            nn.ReLU(True)
-        )
-        self.deconv_out = nn.Sequential(
-            nn.ConvTranspose2d(dim, 1, 8, stride=2),
-            nn.Sigmoid()
-        )
-        self.dim = dim
 
-    def forward(self, input):
-        output = self.preprocess(input)
-        output = output.view(-1, 4 * self.dim, 4, 4)
-        output = self.block1(output)
-        output = output[:, :, :7, :7]
-        output = self.block2(output)
-        output = self.deconv_out(output)
-        return output
+    def forward(self, z):
+        return self.main(z[:, :, None, None])
 
 
 class Discriminator(nn.Module):
     def __init__(self, dim=64):
         super(Discriminator, self).__init__()
         self.main = nn.Sequential(
-            nn.Conv2d(1, dim, 5, stride=2, padding=2),
-            nn.ReLU(True),
-            nn.Conv2d(dim, 2 * dim, 5, stride=2, padding=2),
-            nn.ReLU(True),
-            nn.Conv2d(2 * dim, 4 * dim, 5, stride=2, padding=2),
-            nn.ReLU(True)
+            nn.Conv2d(1, dim, 4, 2, 1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(dim, dim * 2, 4, 2, 1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(dim * 2, dim * 4, 4, 2, 1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(dim * 4, 1, 4, 1, 0)
         )
-        self.output = nn.Linear(4 * 4 * 4 * dim, 1)
-        self.dim = dim
 
-    def forward(self, input):
-        out = self.main(input)
-        out = out.view(-1, 4 * 4 * 4 * self.dim)
-        out = self.output(out)
-        return out.view(-1)
+    def forward(self, x):
+        return self.main(x)
 
 
 class Classifier(nn.Module):
     def __init__(self, z_dim=128, dim=64):
         super(Classifier, self).__init__()
         self.main = nn.Sequential(
-            nn.Conv2d(1, dim, 5, stride=2, padding=2),
-            nn.ReLU(True),
-            nn.Conv2d(dim, 2 * dim, 5, stride=2, padding=2),
-            nn.ReLU(True),
-            nn.Conv2d(2 * dim, 4 * dim, 5, stride=2, padding=2),
-            nn.ReLU(True)
+            nn.Conv2d(1, dim, 4, 2, 1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(dim, dim * 2, 4, 2, 1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(dim * 2, dim * 4, 4, 2, 1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(dim * 4, z_dim, 4, 1, 0)
         )
-        self.mlp1 = nn.Sequential(
-            nn.Linear(4 * 4 * 4 * dim, z_dim),
-            nn.ReLU(True)
+        self.mlp = nn.Sequential(
+            nn.Linear(z_dim * 2, dim * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(dim * 4, 1)
         )
-        self.mlp2 = nn.Sequential(
-            nn.Linear(z_dim * 2, dim),
-            nn.ReLU(True),
-            nn.Linear(dim, 1)
-        )
-        self.dim = dim
 
     def forward(self, x, z):
-        out = self.main(x)
-        out = out.view(-1, 4 * 4 * 4 * self.dim)
-        out = self.mlp1(out)
+        out = self.main(x).squeeze()
         out = torch.cat([out, z], -1)
-        return self.mlp2(out)
+        return self.mlp(out)
 
 
 if __name__ == '__main__':
