42904ddce99407a0f0b4ca3530e57cc61de59a20
Rithesh Kumar <ritheshkumar.95@gmail.com>
2018-09-13 09:36:44 -0400

    improving IS code

--- eval.py
+++ eval.py
@@ -5,22 +5,33 @@ from tqdm import tqdm
 import sys
 
 
-def compute_inception_score(netG):
+def tf_inception_score(netG):
     from inception_score import get_inception_score
     all_samples = []
-    for i in tqdm(range(50)):
+    for i in tqdm(range(500)):
         samples_100 = torch.randn(100, 128).cuda()
         all_samples.append(
             netG(samples_100).detach().cpu().numpy()
         )
 
     all_samples = np.concatenate(all_samples, axis=0)
-    all_samples = (((all_samples * .5) + .5) * 255).astype('int32')
-    all_samples = all_samples.reshape((-1, 3, 32, 32)).transpose(0, 2, 3, 1)
-    return get_inception_score(list(all_samples))
+    return get_inception_score(all_samples)
+
+
+def pytorch_inception_score(netG):
+    from inception_score_pytorch import inception_score
+    all_samples = []
+    for i in tqdm(range(50)):
+        samples_100 = torch.randn(100, 128).cuda()
+        all_samples.append(
+            netG(samples_100).detach().cpu()
+        )
+
+    all_samples = torch.cat(all_samples, 0)
+    return inception_score(all_samples, resize=True, splits=10)
 
 
 netG = Generator().cuda()
 netG.eval()
 netG.load_state_dict(torch.load(sys.argv[1]))
-print(compute_inception_score(netG))
+print(tf_inception_score(netG))
