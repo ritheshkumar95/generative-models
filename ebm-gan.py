c34dea5e11555fd6f7f02c06c4f1d37757ab43b2
Rithesh Kumar <ritheshkumar.95@gmail.com>
2018-09-24 15:49:02 -0400

    reverting master

--- ebm-gan.py
+++ ebm-gan.py
@@ -12,61 +12,9 @@ from data import inf_train_gen
 
 
 def train_generator(netG, netD, optimizerG, optimizerD, g_costs, lamda):
-    netG.zero_grad()
-    netD.zero_grad()
-
-    z = torch.randn(args.batch_size, args.z_dim).cuda()
-    x_fake = netG(z)
-    D_fake = netE(x_fake)
-    D_fake = D_fake.mean()
-    D_fake.backward(retain_graph=True)
-
-    ##########################################
-    # DeepInfoMAX for MI estimation
-    ##########################################
-    label = torch.zeros(2 * args.batch_size).cuda()
-    label[:args.batch_size].data.fill_(1)
-
-    z_bar = z[torch.randperm(args.batch_size)]
-    joint = torch.cat([x_fake, z], -1)
-    marginal = torch.cat([x_fake, z_bar], -1)
-    mi_estimate = nn.BCEWithLogitsLoss()(
-        netD(torch.cat([joint, marginal], 0)).squeeze(),
-        label
-    )
-    (lamda * mi_estimate).backward()
-
-    optimizerG.step()
-    if optimizerD is not None:
-        optimizerD.step()
-
-    g_costs.append(
-        [D_fake.item(), mi_estimate.item()]
-    )
 
 
 def train_energy(netG, netE, optimizerE, d_costs):
-    x_real = torch.from_numpy(itr.__next__()).cuda()
-
-    netE.zero_grad()
-    D_real = netE(x_real)
-    D_real = D_real.mean()
-    D_real.backward()
-
-    # train with fake
-    z = torch.randn(args.batch_size, args.z_dim).cuda()
-    x_fake = netG(z).detach()
-    D_fake = netE(x_fake)
-    D_fake = D_fake.mean()
-    (-D_fake).backward()
-
-    penalty = calc_penalty(netE, x_real, args.lamda)
-    penalty.backward()
-
-    optimizerE.step()
-    d_costs.append(
-        [D_real.item(), D_fake.item(), penalty.item()]
-    )
 
 
 def parse_args():
@@ -99,28 +47,72 @@ plt.clf()
 plt.scatter(orig_data[:, 0], orig_data[:, 1])
 plt.savefig('orig_samples_%s.png' % args.dataset)
 
-netG_le = MLP_Generator(args.input_dim, args.z_dim, args.dim).cuda()
-netG_he = MLP_Generator(args.input_dim, args.z_dim, args.dim).cuda()
+netG = MLP_Generator(args.input_dim, args.z_dim, args.dim).cuda()
 netE = MLP_Discriminator(args.input_dim, args.dim).cuda()
 netD = MLP_Classifier(args.input_dim, args.z_dim, args.dim).cuda()
 
 optimizerE = torch.optim.Adam(netE.parameters(), lr=1e-4, betas=(0.5, 0.9))
 optimizerD = torch.optim.Adam(netD.parameters(), lr=1e-4, betas=(0.5, 0.9))
-optimizerG_le = torch.optim.Adam(netG_le.parameters(), lr=1e-4, betas=(0.5, 0.9))
-optimizerG_he = torch.optim.Adam(netG_he.parameters(), lr=1e-4, betas=(0.5, 0.9))
-
-schedule = np.linspace(1., 0.001, 25000).tolist() + [.001] * (args.iters - 25000)
+optimizerG = torch.optim.Adam(netG.parameters(), lr=1e-4, betas=(0.5, 0.9))
 
 start_time = time.time()
 d_costs = []
-g_le_costs = []
-g_he_costs = []
+g_costs = []
 for iters in range(args.iters):
-    train_generator(netG_he, netD, optimizerG_he, optimizerD, g_he_costs, 1.)
-    train_generator(netG_le, netD, optimizerG_le, None, g_le_costs, schedule[iters])
+    netG.zero_grad()
+    netD.zero_grad()
+
+    z = torch.randn(args.batch_size, args.z_dim).cuda()
+    x_fake = netG(z)
+    D_fake = netE(x_fake)
+    D_fake = D_fake.mean()
+    D_fake.backward(retain_graph=True)
+
+    ##########################################
+    # DeepInfoMAX for MI estimation
+    ##########################################
+    label = torch.zeros(2 * args.batch_size).cuda()
+    label[:args.batch_size].data.fill_(1)
+
+    z_bar = z[torch.randperm(args.batch_size)]
+    joint = torch.cat([x_fake, z], -1)
+    marginal = torch.cat([x_fake, z_bar], -1)
+    mi_estimate = nn.BCEWithLogitsLoss()(
+        netD(torch.cat([joint, marginal], 0)).squeeze(),
+        label
+    )
+    mi_estimate.backward()
+
+    optimizerG.step()
+    if optimizerD is not None:
+        optimizerD.step()
+
+    g_costs.append(
+        [D_fake.item(), mi_estimate.item()]
+    )
 
     for i in range(args.critic_iters):
-        train_energy(netG_he, netE, optimizerE, d_costs)
+        x_real = torch.from_numpy(itr.__next__()).cuda()
+
+        netE.zero_grad()
+        D_real = netE(x_real)
+        D_real = D_real.mean()
+        D_real.backward()
+
+        # train with fake
+        z = torch.randn(args.batch_size, args.z_dim).cuda()
+        x_fake = netG(z).detach()
+        D_fake = netE(x_fake)
+        D_fake = D_fake.mean()
+        (-D_fake).backward()
+
+        penalty = calc_penalty(netE, x_real, args.lamda)
+        penalty.backward()
+
+        optimizerE.step()
+        d_costs.append(
+            [D_real.item(), D_fake.item(), penalty.item()]
+        )
 
     if iters % 100 == 0:
         print('Train Iter: {}/{} ({:.0f}%)\t'
