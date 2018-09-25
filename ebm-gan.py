d80546e29a19efabf9aaa3e9967e554f3e015acf
Rithesh Kumar <ritheshkumar.95@gmail.com>
2018-09-25 00:16:04 -0400

    editing back cifar10

--- ebm-gan.py
+++ ebm-gan.py
@@ -34,12 +34,35 @@ def sample(netG, batch_size=64):
     save_image(x_fake, 'samples/ebm_%s.png' % args.dataset, normalize=True)
 
 
+def sample_negatives(n_steps):
+    z = torch.randn(args.batch_size, args.z_dim).cuda()
+
+    for i in range(n_steps):
+        z.requires_grad_(True)
+        x = netG(z)
+        e_x = netE(x)
+
+        score = torch.autograd.grad(
+            outputs=e_x, inputs=z,
+            grad_outputs=torch.ones_like(e_x),
+            only_inputs=True
+        )[0]
+
+        noise = torch.randn_like(z) * np.sqrt(args.alpha * 2)
+        z = (z - args.alpha * score + noise).detach()
+    return z
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--batch_size', type=int, default=64)
     parser.add_argument('--iters', type=int, default=100000)
-    parser.add_argument('--critic_iters', type=int, default=5)
+
+    parser.add_argument('--mcmc_iters', type=int, default=0)
+    parser.add_argument('--critic_iters', type=int, default=1)
+    parser.add_argument('--generator_iters', type=int, default=5)
     parser.add_argument('--lamda', type=float, default=10)
+    parser.add_argument('--alpha', type=float, default=.01)
 
     parser.add_argument('--z_dim', type=int, default=128)
     parser.add_argument('--dim', type=int, default=512)
@@ -64,47 +87,45 @@ print(netG)
 print(netE)
 print(netD)
 
-optimizerG = torch.optim.Adam(netG.parameters(), lr=1e-4, betas=(0.5, 0.9))
-optimizerD = torch.optim.Adam(netD.parameters(), lr=1e-4, betas=(0.5, 0.9))
-optimizerE = torch.optim.Adam(netE.parameters(), lr=1e-4, betas=(0.5, 0.9))
-
-one = torch.tensor(1., dtype=torch.float32).cuda()
-mone = one * -1
-
-label = torch.ones(2 * args.batch_size).float().cuda()
-label[args.batch_size:].zero_()
+optimizerG = torch.optim.Adam(netG.parameters(), lr=1e-4, betas=(0.5, 0.999), amsgrad=True)
+optimizerD = torch.optim.Adam(netD.parameters(), lr=1e-4, betas=(0.5, 0.999), amsgrad=True)
+optimizerE = torch.optim.Adam(netE.parameters(), lr=1e-4, betas=(0.5, 0.999), amsgrad=True)
 
 start_time = time.time()
 d_costs = []
 g_costs = []
-for iters in range(1, args.iters):
+for iters in range(args.iters):
     netG.zero_grad()
     netD.zero_grad()
 
-    z = torch.randn(args.batch_size, args.z_dim).cuda()
-    x_fake = netG(z)
-    D_fake = netE(x_fake)
-    D_fake = D_fake.mean()
-    D_fake.backward(one, retain_graph=True)
+    for i in range(args.generator_iters):
+        z = torch.randn(args.batch_size, args.z_dim).cuda()
+        x_fake = netG(z)
+        D_fake = netE(x_fake)
+        D_fake = D_fake.mean()
+        D_fake.backward(retain_graph=True)
 
-    ##########################################
-    # DeepInfoMAX for MI estimation
-    ##########################################
-    z_bar = z[torch.randperm(args.batch_size)]
-    concat_x = torch.cat([x_fake, x_fake], 0)
-    concat_z = torch.cat([z, z_bar], 0)
-    mi_estimate = nn.BCEWithLogitsLoss()(
-        netD(concat_x, concat_z).squeeze(),
-        label
-    )
-    mi_estimate.backward()
+        ##########################################
+        # DeepInfoMAX for MI estimation
+        ##########################################
+        label = torch.zeros(2 * args.batch_size).cuda()
+        label[:args.batch_size].data.fill_(1)
 
-    optimizerG.step()
-    optimizerD.step()
+        z_bar = z[torch.randperm(args.batch_size)]
+        concat_x = torch.cat([x_fake, x_fake], 0)
+        concat_z = torch.cat([z, z_bar], 0)
+        mi_estimate = nn.BCEWithLogitsLoss()(
+            netD(concat_x, concat_z).squeeze(),
+            label
+        )
+        mi_estimate.backward()
 
-    g_costs.append(
-        [D_fake.item(), mi_estimate.item()]
-    )
+        optimizerG.step()
+        optimizerD.step()
+
+        g_costs.append(
+            [D_fake.item(), mi_estimate.item()]
+        )
 
     for i in range(args.critic_iters):
         x_real = itr.__next__().cuda()
@@ -112,14 +133,14 @@ for iters in range(1, args.iters):
         netE.zero_grad()
         D_real = netE(x_real)
         D_real = D_real.mean()
-        D_real.backward(one)
+        D_real.backward()
 
         # train with fake
-        z = torch.randn(args.batch_size, args.z_dim).cuda()
+        z = sample_negatives(args.mcmc_iters)
         x_fake = netG(z).detach()
         D_fake = netE(x_fake)
         D_fake = D_fake.mean()
-        D_fake.backward(mone)
+        (-D_fake).backward()
 
         penalty = calc_penalty(netE, x_real, args.lamda)
         penalty.backward()
